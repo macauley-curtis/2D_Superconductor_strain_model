4574a4f7462c03000873271700ae6a0dc6c2664d
Macauley Curtis <88098035+macauley-curtis@users.noreply.github.com>
2023-02-25 21:46:28 +0000

    Update Plot.py
    Changed Plot s.t. it now calls correct titles and plots both components of two component models in DOS

--- Plot.py
+++ Plot.py
@@ -1,34 +1,83 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def gap_titles(gap_pairing):
+    "Returns the interaction potential(s) and the gap pairing(s)"
+    if gap_pairing == "s_wave":
+        return "s-wave", "one component", 0, 0
+    if gap_pairing == "d_wave":
+        return "d$_{x^2-y^2}$", "one component", 0, 0
+    if gap_pairing == "ext_s_wave":
+        return "s$^{\pm}$", "one component", 0, 0
+    if gap_pairing == "d_plus_id":
+        return "d+id", "two component", "$d_{x^2-y^2}$", "$d_{xy}$"
+    if gap_pairing == "d_plus_ig":
+        return "d+ig", "two component", "$d_{x^2-y^2}$", "g"
+    if gap_pairing == "chiral_p":
+        return "p$_x$ + ip$_y$", "two component", "p$_x$", "p$_y$"
+
 
 def plot(strain, gap_pairing, plot_dos, plot_tc, tc_inset):
     "Calls plotting functions and assigns the outputs then saves"
+    full_title, gap_type, x_title, y_title = gap_titles(gap_pairing)
 
-    if plot_dos:
-        ax1, ax2 = dos_plot(strain, gap_pairing)
-        ax1.set_title("{}".format(gap_pairing), fontsize=32)
-        plt.savefig("{}_{}_dos_weighted.jpg".format(gap_pairing, strain))
+    if gap_type == "one component":
+        if plot_dos:
+            ax1, ax2 = dos_plot(strain, gap_pairing)
+            if gap_pairing == "s_wave":
+                pass
+            else:
+                ax1.set_title("{}".format(full_title), fontsize=32)
+            plt.savefig("{}_{}_dos_weighted.jpg".format(gap_pairing, strain))
 
-    if plot_tc:
-        ax1 = tc_plot(strain, gap_pairing, tc_inset)
-        ax1.set_title("{}".format(gap_pairing), fontsize=32)
-        plt.savefig("{}_tc_{}.jpg".format(gap_pairing, strain))
+        if plot_tc:
+            ax1 = tc_plot(strain, gap_pairing, tc_inset)
+            ax1.set_title("{}".format(full_title), fontsize=32)
+            plt.savefig("{}_tc_{}.jpg".format(gap_pairing, strain))
 
+    if gap_type == "two component":
+        if plot_dos:
+            ax1, ax2, ax3, ax4 = dos_plot(strain, gap_pairing)
+            plt.figure(1)
+            ax1.set_title("{}".format(x_title), fontsize=32)
+            plt.savefig("{}_{}_x_dos_weighted.jpg".format(gap_pairing, strain))
+            plt.figure(2)
+            ax3.set_title("{}".format(y_title), fontsize=32)
+            plt.savefig("{}_{}_y_dos_weighted.jpg".format(gap_pairing, strain))
+
+
+        if plot_tc:
+            ax1 = tc_plot(strain, gap_pairing, tc_inset)
+            ax1.set_title("{}".format(full_title), fontsize=32)
+            plt.savefig("{}_tc_{}.jpg".format(gap_pairing, strain))
 
 
 
 def dos_plot(strain, gap_pairing):
     "Returns plots for dos"
 
-    "Load variables"
-    dos = np.load("{}_{}_dos.npy".format(gap_pairing, strain))
-    fermi_surface = np.load("{}_{}_fermi_surface.npy".format(gap_pairing, strain))
+    dummy_var, gap_type, dummy_var, dummy_var = gap_titles(gap_pairing)
+    if gap_type == "one component":
+        "Load variables"
+        dos = np.load("{}_{}_dos.npy".format(gap_pairing, strain))
+        fermi_surface = np.load("{}_{}_fermi_surface.npy".format(gap_pairing, strain))
+        hopping_step, dos_step = np.shape(dos)
+        n_x, n_y = np.shape(fermi_surface[0, :, :])
+
+    if gap_type == "two component":
+        dos_x = np.load("{}_x_dos_{}.npy".format(gap_pairing, strain))
+        dos_y = np.load("{}_y_dos_{}.npy".format(gap_pairing, strain))
+        fermi_surface_x = np.load("{}_x_fermi_surface_{}.npy".format(gap_pairing, strain))
+        fermi_surface_y = np.load("{}_y_fermi_surface_{}.npy".format(gap_pairing, strain))
+        hopping_step, dos_step = np.shape(dos_x)
+        n_x, n_y = np.shape(fermi_surface_x[0, :, :])
+
+
+
+
     hopping_array = np.load("hopping_array_{}.npy".format(strain))
 
     "create physical arrays and assign array size based on input arrays "
-    hopping_step, dos_step = np.shape(dos)
-    n_x, n_y = np.shape(fermi_surface[0, :, :])
     t_x = 81.62 * 10 ** -3
     epsilon = np.linspace(-2 * t_x, 2.5 * t_x,
                           dos_step) * 10 ** 3  # This is hard coded in for now, *MUST* match epsilon used for dos calculation
@@ -42,42 +91,95 @@ def dos_plot(strain, gap_pairing):
     t_x_2 = round(hopping_array[n_2] / hopping_array[0], 2)
     t_x_end = round(hopping_array[-1] / hopping_array[0], 2)
 
-    " Create figure and axis sizes "
-    fig, ax1 = plt.subplots(figsize=(8, 8))
-    # These are in unitless percentages of the figure size. (0,0 is bottom left)
-    left, bottom, width, height = [0.15, 0.61, 0.225, 0.225]
-    ax2 = fig.add_axes([left, bottom, width, height])
+    if gap_type == "one component":
+        " Create figure and axis sizes "
+        fig, ax1 = plt.subplots(figsize=(8, 8))
+        # These are in unitless percentages of the figure size. (0,0 is bottom left)
+        left, bottom, width, height = [0.15, 0.61, 0.225, 0.225]
+        ax2 = fig.add_axes([left, bottom, width, height])
 
-    "Plot ax1, dos vs energy at various strains"
-    ax1.plot(epsilon[:], dos[0, :], 'b.', label=(hopping_array[0] / hopping_array[0]))
-    ax1.plot(epsilon[:], dos[n_1, :], 'g.', label=(t_x_1))
-    ax1.plot(epsilon[:], dos[n_2, :], 'r.', label=(t_x_2))
-    ax1.plot(epsilon[:], dos[-1, :], 'm.', label=(t_x_end))
-    ax1.set_xticks((-t_x * 10 ** 3, 0, t_x * 10 ** 3), ['$-t_0$', '$\epsilon_{f}$', '$t_0$'])
-    ax1.xaxis.set_tick_params(labelsize=28)
-    ax1.set_xlabel("$\\varepsilon$ / meV", fontsize=32)
-    ax1.set_ylabel("$\\rho(\\varepsilon)$", fontsize=36)
-    ax1.legend(loc='upper right', title='$\\frac{t_x}{t_0}$', prop={'size': 24}, title_fontsize=28, markerscale=4)
+        "Plot ax1, dos vs energy at various strains"
+        ax1.plot(epsilon[:], dos[0, :], 'b.', label=(hopping_array[0] / hopping_array[0]))
+        ax1.plot(epsilon[:], dos[n_1, :], 'g.', label=(t_x_1))
+        ax1.plot(epsilon[:], dos[n_2, :], 'r.', label=(t_x_2))
+        ax1.plot(epsilon[:], dos[-1, :], 'm.', label=(t_x_end))
+        ax1.set_xticks((-t_x * 10 ** 3, 0, t_x * 10 ** 3), ['$-t_0$', '$\epsilon_{f}$', '$t_0$'])
+        ax1.xaxis.set_tick_params(labelsize=28)
+        ax1.set_xlabel("$\\varepsilon$ / meV", fontsize=32)
+        ax1.set_ylabel("$\\rho(\\varepsilon)$", fontsize=36)
+        ax1.legend(loc='upper right', title='$\\frac{t_x}{t_0}$', prop={'size': 24}, title_fontsize=28, markerscale=4)
 
-    "ax2 plot, Fermi-surface inset"
+        "ax2 plot, Fermi-surface inset"
 
-    ax2.contour(k_x, k_y, fermi_surface[0, :, :], colors='blue')
-    ax2.contour(k_x, k_y, fermi_surface[1, :, :], colors='red')
-    ax2.xaxis.set_tick_params(labelsize=15, labeltop=True, labelbottom=False)
-    ax2.yaxis.set_tick_params(labelsize=15, labelright=True, labelleft=False)
-    ax2.set_xticks((-np.pi, 0, np.pi), ['$-\pi$', '0', '$\pi$'])
-    ax2.set_yticks((np.pi, 0, -np.pi), ['$\pi$', '0', '$-\pi$'])
+        ax2.contour(k_x, k_y, fermi_surface[0, :, :], colors='blue')
+        ax2.contour(k_x, k_y, fermi_surface[1, :, :], colors='red')
+        ax2.xaxis.set_tick_params(labelsize=15, labeltop=True, labelbottom=False)
+        ax2.yaxis.set_tick_params(labelsize=15, labelright=True, labelleft=False)
+        ax2.set_xticks((-np.pi, 0, np.pi), ['$-\pi$', '0', '$\pi$'])
+        ax2.set_yticks((np.pi, 0, -np.pi), ['$\pi$', '0', '$-\pi$'])
 
-    return ax1, ax2
+        return ax1, ax2
+
+    if gap_type == "two component":
+        " Create figure and axis sizes "
+        fig1, ax1 = plt.subplots(figsize=(8, 8))
+        # These are in unitless percentages of the figure size. (0,0 is bottom left)
+        left, bottom, width, height = [0.635, 0.2, 0.225, 0.225]
+        ax2 = fig1.add_axes([left, bottom, width, height])
+
+        "Plot ax1, dos vs energy at various strains"
+        ax1.plot(epsilon[:], dos_x[0, :], 'b.', label=(hopping_array[0] / hopping_array[0]))
+        ax1.plot(epsilon[:], dos_x[n_1, :], 'g.', label=(t_x_1))
+        ax1.plot(epsilon[:], dos_x[n_2, :], 'r.', label=(t_x_2))
+        ax1.plot(epsilon[:], dos_x[-1, :], 'm.', label=(t_x_end))
+        ax1.set_xticks((-t_x * 10 ** 3, 0, t_x * 10 ** 3), ['$-t_0$', '$\epsilon_{f}$', '$t_0$'])
+        ax1.xaxis.set_tick_params(labelsize=28)
+        ax1.set_xlabel("$\\varepsilon$ / meV", fontsize=32)
+        ax1.set_ylabel("$\\rho(\\varepsilon)$", fontsize=36)
+        ax1.legend(loc='upper right', title='$\\frac{t_x}{t_0}$', prop={'size': 24}, title_fontsize=28, markerscale=4)
+
+        "ax2 plot, Fermi-surface inset"
+
+        ax2.contour(k_x, k_y, fermi_surface_x[0, :, :], colors='blue')
+        ax2.contour(k_x, k_y, fermi_surface_x[1, :, :], colors='red')
+        ax2.xaxis.set_tick_params(labelsize=15, labeltop=True, labelbottom=False)
+        ax2.yaxis.set_tick_params(labelsize=15, labelright=True, labelleft=False)
+        ax2.set_xticks((-np.pi, 0, np.pi), ['$-\pi$', '0', '$\pi$'])
+        ax2.set_yticks((np.pi, 0, -np.pi), ['$\pi$', '0', '$-\pi$'])
+
+        " Create figure and axis sizes "
+        fig2, ax3 = plt.subplots(figsize=(8, 8))
+        # These are in unitless percentages of the figure size. (0,0 is bottom left)
+        left, bottom, width, height = [0.635, 0.2, 0.225, 0.225]
+        ax4 = fig2.add_axes([left, bottom, width, height])
+
+        "Plot ax1, dos vs energy at various strains"
+        ax3.plot(epsilon[:], dos_y[0, :], 'b.', label=(hopping_array[0] / hopping_array[0]))
+        ax3.plot(epsilon[:], dos_y[n_1, :], 'g.', label=(t_x_1))
+        ax3.plot(epsilon[:], dos_y[n_2, :], 'r.', label=(t_x_2))
+        ax3.plot(epsilon[:], dos_y[-1, :], 'm.', label=(t_x_end))
+        ax3.set_xticks((-t_x * 10 ** 3, 0, t_x * 10 ** 3), ['$-t_0$', '$\epsilon_{f}$', '$t_0$'])
+        ax3.xaxis.set_tick_params(labelsize=28)
+        ax3.set_xlabel("$\\varepsilon$ / meV", fontsize=32)
+        ax3.set_ylabel("$\\rho(\\varepsilon)$", fontsize=36)
+        ax3.legend(loc='upper right', title='$\\frac{t_x}{t_0}$', prop={'size': 24}, title_fontsize=28, markerscale=4)
+
+        "ax2 plot, Fermi-surface inset"
+
+        ax4.contour(k_x, k_y, fermi_surface_y[0, :, :], colors='blue')
+        ax4.contour(k_x, k_y, fermi_surface_y[1, :, :], colors='red')
+        ax4.xaxis.set_tick_params(labelsize=15, labeltop=True, labelbottom=False)
+        ax4.yaxis.set_tick_params(labelsize=15, labelright=True, labelleft=False)
+        ax4.set_xticks((-np.pi, 0, np.pi), ['$-\pi$', '0', '$\pi$'])
+        ax4.set_yticks((np.pi, 0, -np.pi), ['$\pi$', '0', '$-\pi$'])
+
+        return ax1, ax2, ax3, ax4
 
 def tc_plot(strain, gap_pairing, tc_inset):
     "returns plot for tc"
 
-    "Determine gap type"
-    if gap_pairing == "s_wave" or gap_pairing == "ext_s_wave" or gap_pairing == "d_wave":
-        gap_type = "one component"
-    else:
-        gap_type = "two component"
+    "call title and gap type"
+    title, gap_type, dummy_var, dummy_var = gap_titles(gap_pairing)
 
     "Load variables"
 
@@ -101,11 +203,11 @@ def tc_plot(strain, gap_pairing, tc_inset):
 
     if gap_type == "two component":
 
-        tc_x = np.load("{}_tc_x_{}".format(gap_pairing, strain))
-        tc_y = np.load("{}_tc_y_{}".format(gap_pairing, strain))
+        tc_x = np.load("{}_tc_x_{}.npy".format(gap_pairing, strain))
+        tc_y = np.load("{}_tc_y_{}.npy".format(gap_pairing, strain))
 
         fig, ax1 = plt.subplots(figsize=(8, 8))
-        ax1.plot(hopping_array[:] / hopping_array[0], tc_x[:], 'r-', markersize=16)
+        ax1.plot(hopping_array[:] / hopping_array[0], tc_x[:], 'r.', markersize=16)
         ax1.plot(hopping_array[:] / hopping_array[0], tc_y[:], 'b.', markersize=16)
         ax1.xaxis.set_tick_params(labelsize=15)
         ax1.yaxis.set_tick_params(labelsize=19)
@@ -113,8 +215,8 @@ def tc_plot(strain, gap_pairing, tc_inset):
         ax1.set_xlabel("$t_x / t_0$", fontsize=32)
 
         if tc_inset:
-            delta_x = np.load("{}_delta_x_{}".format(gap_pairing, strain))
-            delta_y = np.load("{}_delta_y_{}".format(gap_pairing, strain))
+            delta_x = np.load("{}_delta_x_{}.npy".format(gap_pairing, strain))
+            delta_y = np.load("{}_delta_y_{}.npy".format(gap_pairing, strain))
             # These are in unitless percentages of the figure size. (0,0 is bottom left)
             left, bottom, width, height = [0.75, 0.45, 0.15, 0.3]
             ax2 = fig.add_axes([left, bottom, width, height])
